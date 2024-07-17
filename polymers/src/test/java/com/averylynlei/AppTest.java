7e9b902e376e3cfb00ff037ac02c65cc10434161
avery lyn lei <averylei@outlook.com>
2024-07-17 09:50:29 -0600

    add project template!

---
+++ polymers/src/test/java/com/averylynlei/AppTest.java
@@ -0,0 +1,20 @@
+package com.averylynlei;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
